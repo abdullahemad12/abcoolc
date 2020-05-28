beaedd77e5e4f524da845c9b4eeb10bee979dae7
Abdullah Emad <abdullahem1997@hotmail.com>
2020-05-29 01:08:48 +0200

    Add unit testing compilation script to Makefile

---
+++ src/analyzer/test.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "cool-tree.h"
+
+extern Program ast_root;      // root of the abstract syntax tree
+FILE *ast_file = stdin;       // we read the AST from standard input
+extern int ast_yyparse(void); // entry point to the AST parser
+
+int cool_yydebug;     // not used, but needed to link with handle_flags
+char *curr_filename;
+
+void handle_flags(int argc, char *argv[]);
+
+using namespace std;
+
+int main() 
+{
+	cout << "help!" << "\n";
+}
