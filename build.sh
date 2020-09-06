493b853faab429cc3d2f339f948c8539ab294d4c
Abdullah Emad <abdullahem1997@hotmail.com>
2020-09-06 13:45:53 +0200

    Modify gitignore

---
+++ build.sh
@@ -0,0 +1,7 @@
+##################################
+# Compiles the components of the
+# Cool program and copies them 
+# to bin directory
+###################################
+
+make lexer -C src/lexer
