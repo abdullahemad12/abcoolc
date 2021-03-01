9e3166b1791ba0ab26bd86423c5f791a3eecb243
Abdullah Emad <rkmelody97@yahoo.com>
2021-03-01 19:47:23 +0200

    Clean parser

--- src/parser/cool-parse.d
+++ src/parser/cool-parse.d
@@ -3,4 +3,5 @@ cool-parse.o cool-parse.d : cool-parse.cc ../../include/parser/cool-tree.h \
  ../../include/parser/stringtab.h ../../include/parser/list.h \
  ../../include/parser/cool-io.h cool-tree.handcode.h \
  ../../include/parser/tree.h ../../include/parser/cool.h \
- ../../include/parser/stringtab.h ../../include/parser/utilities.h
+ ../../include/parser/stringtab.h ../../include/parser/utilities.h \
+ cool.tab.h
